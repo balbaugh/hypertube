28ffbaaea790a3145046a10db376080f758d0b5e
Brian Albaugh <balbaugh@gmail.com>
2023-02-12 13:07:54 +0200

    Updates -- Merge

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,19 @@
+
 SET timezone = 'Europe/Helsinki';
 
-CREATE TABLE IF NOT EXISTS users (
-	id SERIAL NOT NULL PRIMARY KEY,
-	username VARCHAR(255) NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS movies (
-	id SERIAL NOT NULL PRIMARY KEY,
-	movie_path VARCHAR(255) NOT NULL,
-	size VARCHAR(255) NOT NULL,
-	downloaded BOOLEAN DEFAULT false NOT NULL,
-	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS users ( id SERIAL NOT NULL PRIMARY KEY,
+																																			email VARCHAR(255) NOT NULL,
+																																			username VARCHAR(255) NOT NULL,
+																																			firstname VARCHAR(255) NOT NULL,
+																																			lastname VARCHAR(255) NOT NULL,
+																																			password VARCHAR(255) NOT NULL,
+																																			verifycode VARCHAR(255) NOT NULL,
+																																			status INT DEFAULT 0 NOT NULL);
+
+
+CREATE TABLE IF NOT EXISTS movies ( id SERIAL NOT NULL PRIMARY KEY,
+																																				movie_path VARCHAR(255) NOT NULL,
+																																				size VARCHAR(255) NOT NULL,
+																																				downloaded BOOLEAN DEFAULT false NOT NULL, date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-const config = require('./utils/config');
 const express = require('express');
 const cors = require('cors');
 const torrentStream = require('torrent-stream');
@@ -8,59 +7,77 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
+const crypto = require('crypto')
+const bcrypt = require('bcryptjs');
+const config = require('./utils/config');
+
 const app = express();
 app.use(express.json());
 app.use(cors({
-	origin: ['http://localhost:3000'],
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	credentials: true
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 }));
 
 app.use(express.static('downloads'));
 
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-	// key: 'userID',
-	secret: 'BIGSECRET',
-	resave: false,
-	saveUninitialized: false,
-	cookie: {
-		sameSite: 'none',
-		secure: 'true'
+  key: 'userID',
+  secret: 'BIGSECRET',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 1000 * 60 * 60 * 24,
+    sameSite: 'Lax',
 	}
-}))
+}));
 
 const dbConn = require('./utils/dbConnection');
+
 const { connectDB } = dbConn;
 connectDB();
 
 const middleware = require('./utils/middleware');
+
 app.use(middleware.requestLogger);
 app.use(middleware.morganLogger);
 
 app.get('/', (req, res) => {
-	res.sendStatus(200).end()
+  res.sendStatus(200).end();
 });
 
 app.get('/testdb', (req, res) => {
-	dbConn.pool.query('SELECT * FROM users',
-		(err, result) => {
-			if (err)
-				console.log(err)
-			else {
-				res.send(result)
-			}
-		})
-})
+  dbConn.pool.query(
+'SELECT * FROM users',
+    (err, result) => {
+      if (err) {console.log(err)};
+      else {
+        res.send(result);
+      }
+    }
+);
+});
 
 const movies = require('./routes/movie');
+
 app.use(movies);
 
 const video = require('./routes/video');
+
 app.use(video);
+
+const reg = require('./routes/register');
+
+app.use(reg);
+
+const login = require('./routes/login');
+
+app.use(login);
 
 app.use(middleware.unknowEndpoint);
 
 app.listen(config.PORT, () => {
-	console.log(`Server on port ${config.PORT}`)
-})
+  console.log(`Server on port ${config.PORT}`);
+});
