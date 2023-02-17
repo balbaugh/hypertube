c1426135851833f99d96176424f63181942f873c
Brian Albaugh <balbaugh@gmail.com>
2023-02-17 12:26:01 +0200

    language save state

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,10 +14,14 @@ const Nav = ({ itsMe, setItsMe, selectedAvatar }) => {
 
 	const changeLanguage = (lng) => {
 		i18n.changeLanguage(lng);
+		localStorage.setItem('language', lng); // store selected language in local storage
 	};
 
 	useEffect(() => {
-		
+		const savedLanguage = localStorage.getItem('language');
+		if (savedLanguage) {
+			i18n.changeLanguage(savedLanguage);
+		}
 	}, [selectedAvatar]);
 
 	axios.defaults.withCredentials = true; // For the sessions the work
@@ -91,7 +95,7 @@ const Nav = ({ itsMe, setItsMe, selectedAvatar }) => {
 									<>
 										<a
 											href="/logout"
-											className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+											className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-red-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 										>
 											<span className="sr-only">{t('Navbar.SignOut')}</span>
 											<PowerIcon className="h-6 w-6" aria-hidden="true" />
@@ -101,7 +105,7 @@ const Nav = ({ itsMe, setItsMe, selectedAvatar }) => {
 										{/* Language Dropdown */}
 										<Menu as="div" className="relative ml-4">
 											<div>
-												<Menu.Button className="flex rounded-full text-gray-400 hover:text-white bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+												<Menu.Button className="flex rounded-full text-gray-400 hover:text-lime-500 bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
 													<span className="sr-only">Open language menu</span>
 													<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
 														<path strokeLinecap="round" strokeLinejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
@@ -179,7 +183,7 @@ const Nav = ({ itsMe, setItsMe, selectedAvatar }) => {
 													<Menu.Item>
 														{({ active }) => (
 															<a
-																href="/settings"
+																href="/profileEdit"
 																className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
 															>
 																{t('Navbar.Settings')}
@@ -282,129 +286,3 @@ const Nav = ({ itsMe, setItsMe, selectedAvatar }) => {
 	);
 };
 export default Nav;
-
-
-// <Navbar
-// 	className="bg-zinc-900 border-b-[3px] border-zinc-700"
-// 	fluid={true}
-// 	id="navbar"
-// 	style={{ backgroundColor: '#19191c', borderColor: '#3F3F46' }}
-// >
-// 	<Navbar.Brand as={Link} to="/">
-// 		<img
-// 			src={require('../images/hypertubeLogo.png')}
-// 			className="h-12 mr-3 sm:h-9"
-// 			alt="Hypertube Logo"
-// 		/>
-// 	</Navbar.Brand>
-// 	{itsMe.username || itsMe.login ? (
-// 		<>
-// 			<div className="flex">
-// 				{/* Logout Button */ }
-// 				<Link className="mr-2 mt-2" to="/logout">
-// 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-// 						 stroke="#f05252" className="w-8 h-8">
-// 						<path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"/>
-// 					</svg>
-// 				</Link>
-//
-// 				{/* Menu Toggle */ }
-// 				<Navbar.Toggle className="mb-1 ml-5 border-2 border-red-500 hover:bg-transparent text-red-500 hover:text-red-500 focus:text-red-500 focus:bg-transparent focus:border-red-500" />
-//
-// 				<Navbar.Collapse>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/homepage"
-// 						style={{ color: "#f05252" }}
-// 						// active={true}
-// 					>
-// 						Home
-// 					</Navbar.Link>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/landing"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Landing
-// 					</Navbar.Link>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/login"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Login
-// 					</Navbar.Link>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/registration"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Registration
-// 					</Navbar.Link>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/forgot"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Forgot
-// 					</Navbar.Link>
-// 				</Navbar.Collapse>
-// 			</div>
-// 		</>
-// 	) : (
-// 		<>
-// 			<div className="flex">
-// 				{/* Logout Button */ }
-// 				<Link className="mr-2 mt-2" to="/logout">
-// 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-// 						 stroke="#f05252" className="w-8 h-8">
-// 						<path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"/>
-// 					</svg>
-// 				</Link>
-// 			</div>
-//
-//
-// 				{/* Menu Toggle */ }
-// 				<Navbar.Toggle className="mb-1 ml-1 border-2 border-red-500 hover:bg-transparent text-red-500 hover:text-red-500 focus:text-red-500 focus:bg-transparent focus:border-red-500" />
-//
-// 				<Navbar.Collapse>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/homepage"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Home
-// 					</Navbar.Link>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/landing"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Landing
-// 					</Navbar.Link>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/login"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Login
-// 					</Navbar.Link>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/registration"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Registration
-// 					</Navbar.Link>
-// 					<Navbar.Link
-// 						className="font-medium hover:bg-zinc-800 hover:text-red-600"
-// 						href="/forgot"
-// 						style={{ color: "#f05252" }}
-// 					>
-// 						Forgot
-// 					</Navbar.Link>
-// 				</Navbar.Collapse>
-//
-// 		</>
-// 	)}
-// </Navbar>
