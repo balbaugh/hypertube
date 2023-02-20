e00f5e6bc4a8cb881e3c913a81d4bf1922fc34d5
Brian Albaugh <balbaugh@gmail.com>
2023-02-20 13:12:46 +0200

    error squashing

--- client/src/components/FilmDetail.jsx
+++ client/src/components/FilmDetail.jsx
@@ -424,9 +424,6 @@ const FilmDetail = ({itsMe}) => {
                                                                         <button
                                                                             type="submit"
                                                                             className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-200 bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                                                            onClick={() => {
-                                                                                textInput.current.value = "";
-                                                                            }}
                                                                         >
                                                                             {t('FilmDetail.post')}
                                                                         </button>
